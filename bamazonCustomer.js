91585d9d2002fce1869f102c2b90c70d969f759e
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2019-11-11 20:14:02 -0500

    updating js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,3 +15,23 @@ connection.connect(err => {
     if (err) throw new err;
     console.log("connected as id:" + connection.threadId)
 });
+
+const dasplayProducts = function () {
+    const query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        const displayTable = new table({
+            head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
+            colWidths: [8, 20, 20, 8, 10]
+        });
+        for (const i = 0; i < res.lenght; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        purchasePrompt();
+    })
+
+
+
+}
